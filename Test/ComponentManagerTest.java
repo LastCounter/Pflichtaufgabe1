cfd3bd9990cf4838c9a987b79c590ae4b68a67f3
Acer <Furkan_dogu@hotmail.de>
2022-05-11 19:02:54 +0200

    with JUnitTest class

---
+++ Test/ComponentManagerTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComponentManagerTest {
+
+    @Test
+    void searchComponent() {
+    }
+
+    @Test
+    void calculatePrice() {
+    }
+
+    @Test
+    void testCalculatePrice() {
+    }
+}
